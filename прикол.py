ddf0650392731fa0d9af19cbe084d8c20994d2c2
timka4you <166625613+timka4you@users.noreply.github.com>
2024-05-06 16:28:53 +0300

    prikol

---
+++ прикол.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+from subprocess import run
+
+
+def bluescreen():
+    root = tk.Tk()
+    root.attributes('-fullscreen', True)  # Use full-screen mode
+    root.wm_attributes('-topmost', True)  # Use Sticky mode
+    # The following code can be based on other results they want to achieve changes
+    frame = tk.Frame(root, bg='darkblue')
+    frame.pack(fill='both', expand=True)
+    return root
+
+
+def close_explorer():
+    "" "Close the Explorer process" ""
+    run('taskkill /F /IM explorer.exe')
+
+
+if __name__ == '__main__':
+    root = bluescreen()
+    close_explorer()
+    root.mainloop()
